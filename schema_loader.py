ef16a40be0b70d20cfd81d757f2a57f8e1bd419d
Mohamed <mmaraghi47@gmail.com>
2025-11-15 16:57:32 +0300

    git ignore added

--- schema_loader.py
+++ schema_loader.py
@@ -1,4 +1,4 @@
-def load_schema(schema_file="bank_demo_schema.sql"):
+def load_schema(schema_file="bank_schema.sql"):
     """Read the schema file for LLM context."""
     try:
         with open(schema_file, "r") as f:
